6a5b0dd0b330d4cf27e6199ed72f4188223a21fa
Richard Prange <144190234+rwprange1@users.noreply.github.com>
2026-06-17 23:10:33 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        my_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in my_dict:
+                return True
+
+            my_dict[nums[i]] = 1
+        return False        
