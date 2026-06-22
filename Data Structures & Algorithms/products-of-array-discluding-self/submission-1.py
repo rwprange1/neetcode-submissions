09934291960dc6ef9f1fd4f74c21ad41fce533fd
Richard Prange <144190234+rwprange1@users.noreply.github.com>
2026-06-22 00:24:33 -0400

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        product  = 1 
+        zeros = 0
+
+        for num in nums:
+            if num != 0:
+                product *= num
+            else:
+                zeros += 1
+
+        print(product)
+
+        if zeros >= 2:
+            print("too many z's")
+            return [0] * len(nums)
+
+
+        to_ret = [0] * len(nums)
+        for i in range(len(nums)):
+            if zeros:
+                to_ret[i] = 0 if nums[i] != 0 else product
+            else:
+                to_ret[i] = product // nums[i]
+
+        return to_ret            
+
+
+
+
+
+        
